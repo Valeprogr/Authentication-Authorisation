feb1a49dcb6d0ec9406d2b45b11470573011f035
valentina vittoria <valeviit@icloud.com>
2022-12-07 18:16:01 +0100

    first exercise

--- src/index.js
+++ src/index.js
@@ -9,6 +9,36 @@ const app= express();
 
 app.use(json());
 
+
+const sess = {
+    secret: "boh",
+    resave: false,
+    saveUnitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 240000000000,
+    }
+}
+app.use(session(sess));
+
+app.get("/",(req,res)=>{
+    console.log(req.session)
+    res.send("Hello there, welcome")
+})
+
+app.get("/setname",(req,res)=>{
+    req.session.user ={ name : "Jane Doe"}
+    console.log(req.session.user)
+    res.send(`Welcome ${req.session.user.name} !`)
+    res.end();
+
+})
+
+app.get("/getname",(req,res)=>{
+    res.send(`You still connected ${req.session.user.name}`)
+} )
+
+
 async function AppStart(){
     try{
     await app.listen(PORT,()=>{
